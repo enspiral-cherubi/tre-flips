e1f88b2d205d5b5eb3b911f906f245137804129c
euglazer <eugenestapler@gmail.com>
2016-05-11 23:03:50 -0700

    row of bodies rendering

--- src/engine/environment/index.js
+++ src/engine/environment/index.js
@@ -3,8 +3,8 @@ import $ from 'jquery'
 import ThreeOrbitControls from 'three-orbit-controls'
 var OrbitControls = ThreeOrbitControls(THREE)
 import WindowResize from 'three-window-resize'
-
 import loadBodyMesh from './load-body-mesh.js'
+import range from 'lodash.range'
 
 class Environment {
 
@@ -12,7 +12,7 @@ class Environment {
     this.scene = new THREE.Scene()
 
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.01, 1000)
-    this.camera.position.z = 100
+    this.camera.position.z = 250
 
     this.controls = new OrbitControls(this.camera)
 
@@ -22,30 +22,26 @@ class Environment {
 
     var windowResize = new WindowResize(this.renderer, this.camera)
 
-    loadBodyMesh().then((mesh) => {
-      this.scene.add(mesh)
-    })
+    this._addBodiesToScene(12)
   }
 
   render () {
-    // this._updateCube()
     this.renderer.render(this.scene, this.camera)
   }
 
   // 'private'
 
-  _addCubeToScene () {
-    var geometry = new THREE.BoxGeometry(1, 1, 1)
-  	var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-  	this.cube = new THREE.Mesh(geometry, material)
-  	this.scene.add(this.cube)
+  _addBodiesToScene (bodyCount) {
+    var spacing = 40
+    range(bodyCount).forEach((i) => {
+      loadBodyMesh().then((mesh) => {
+        var x = spacing * i - (spacing * bodyCount / 2)
+        mesh.position.set(x, 0, 0)
+        mesh.rotation.set(Math.PI / 2, 0, Math.PI / 1.2)
+        this.scene.add(mesh)
+      })
+    })
   }
-
-  _updateCube () {
-    this.cube.rotation.x += 0.1
-		this.cube.rotation.y += 0.1
-  }
-
 }
 
 export default Environment
